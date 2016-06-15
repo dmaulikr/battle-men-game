543686666c929936989f20d9cf9f5ed2d0c5bf32
ekingan <ekingan@gmail.com>
2016-06-15 08:28:18 -0700

    layout

--- battle-men-game/ViewController.swift
+++ battle-men-game/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enemyAttackLbl: UILabel!
+    
+    @IBOutlet weak var playerAttackLbl: UILabel!
+    
+    @IBOutlet weak var enemyImg: UIImageView!
+    
+    @IBOutlet weak var playerImg: UIImageView!
+    
+    @IBOutlet weak var printLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
